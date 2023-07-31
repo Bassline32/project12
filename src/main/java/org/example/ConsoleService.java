580f0e508e285ebc66c2c7011dce18dac66edb77
Bassline32 <ivandzen911@Gmail.com>
2023-07-31 22:56:03 +0300

    Отделил бизнес логику в отдельный класс

---
+++ src/main/java/org/example/ConsoleService.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class ConsoleService {
+    public void getUserData() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите ник игрока: ");
+        String nickname = scanner.nextLine();
+        System.out.print("Введите логин игрока: ");
+        String login = scanner.nextLine();
+        System.out.println("Ник игрока: " + nickname);
+        System.out.println("Логин игрока: " + login);
+
+    }
+
+}
+
+
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,9 @@
 package org.example;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите ник игрока: ");
-        String nickname = scanner.nextLine();
-        System.out.print("Введите логин игрока: ");
-        String login = scanner.nextLine();
-        System.out.println("Ник игрока: " + nickname);
-        System.out.println("Логин игрока: " + login);
+        ConsoleService console = new ConsoleService();
+        console.getUserData();
+
     }
 }
